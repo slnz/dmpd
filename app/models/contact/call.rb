41b1e4311689fe779e0335d13214d884ecf8dfcc
Tataihono Nikora <tataihono.nikora@gmail.com>
2014-11-24 11:05:45 +1300

    fix bug where enum was being defined twice

--- app/models/contact/call.rb
+++ app/models/contact/call.rb
@@ -3,12 +3,16 @@ class Contact
     belongs_to :contact
     has_many :events, dependent: :destroy
 
-    enum state: [:not_in, :must_callback, :got_appointment, :no_appointment,
-                 :got_contacts, :no_contacts, :got_support, :no_support,
-                 :answered, :line_busy, :no_answer]
+    unless instance_methods.include? :state
+      enum state: [:not_in, :must_callback, :got_appointment, :no_appointment,
+                   :got_contacts, :no_contacts, :got_support, :no_support,
+                   :answered, :line_busy, :no_answer]
+    end
+    unless instance_methods.include? :step
     enum step: [:init, :start_call, :new_contact,
                 :must_callback_for_contacts, :must_callback_for_decision,
                 :ask_for_contacts, :appointment, :callback, :support,
                 :not_present, :input_contacts, :stats, :end]
+    end
   end
 end
