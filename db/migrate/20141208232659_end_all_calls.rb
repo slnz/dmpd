d6133400063f757d38f59a5ea02bdf0fe48c7d45
Tataihono Nikora <tataihono.nikora@gmail.com>
2015-02-05 10:52:16 +1300

    migration repair

--- db/migrate/20141208232659_end_all_calls.rb
+++
@@ -1,8 +0,0 @@
-class EndAllCalls < ActiveRecord::Migration
-  def change
-    Contact::Call.where(end_time: nil).each do |call|
-      call.end_time = Time.now
-      call.save
-    end
-  end
-end
